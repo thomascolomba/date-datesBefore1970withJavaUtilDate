b9856916848b346b730ac6f4d472d4bb3372783b
thomas <thomas.colomba30@gmail.com>
2020-06-16 10:13:55 +0200

    show ERA for years 0, -1 and -2

--- src/main/java/datesBefore1970withJavaUtilDate/DatesBefore1970.java
+++ src/main/java/datesBefore1970withJavaUtilDate/DatesBefore1970.java
@@ -13,8 +13,8 @@ public class DatesBefore1970 {
 		showYear2();
 		showYear1();
 		showYear0();//note to myself : year 0 does not exist
-		showYearMinus1();//fails
-		showYearMinus2();//fails
+		showYearMinus1();
+		showYearMinus2();
 	}
 	
 	private static void showYear1970() {
@@ -105,50 +105,32 @@ public class DatesBefore1970 {
 			e.printStackTrace();
 		}
 		System.out.println("showYear0");
-		System.out.println(myDate.getYear());
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss G").format(myDate));
 	}
 	
 	private static void showYearMinus1() {
 		String sDate = "-1-01-01 00:00:00";
-		Date myDate1 = null;
-		Date myDate2 = null;
-		Date myDate3 = null;
-		Date myDate4 = null;
+		Date myDate = null;
 		try {
-			myDate1 = new SimpleDateFormat("uuuuu-MM-dd HH:mm:ss").parse(sDate);//goes 1970
-			myDate2 = new SimpleDateFormat("uuuu-MM-dd HH:mm:ss").parse(sDate);//goes 1970
-			myDate3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 2 instead of -1
-			myDate4 = new SimpleDateFormat("yyyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 2 instead of -1
+			myDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 2 instead of -1
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("showYearMinus1");
-		System.out.println(myDate1.getYear());
-		System.out.println(myDate2.getYear());
-		System.out.println(myDate3.getYear());
-		System.out.println(myDate4.getYear());
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss G").format(myDate));
 	}
 	
 	private static void showYearMinus2() {
+		System.out.println("showYearMinus2");
 		String sDate = "-0002-01-01 00:00:00";
-		Date myDate1 = null;
-		Date myDate2 = null;
-		Date myDate3 = null;
-		Date myDate4 = null;
+		Date myDate = null;
 		try {
-			myDate1 = new SimpleDateFormat("uuuuu-MM-dd HH:mm:ss").parse(sDate);//goes 1970
-			myDate2 = new SimpleDateFormat("uuuu-MM-dd HH:mm:ss").parse(sDate);//goes 1970
-			myDate3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 3 instead of -2
-			myDate4 = new SimpleDateFormat("yyyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 3 instead of -2
+			myDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sDate);//goes year 3 instead of -2
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("showYearMinus2");
-		System.out.println(myDate1.getYear());
-		System.out.println(myDate2.getYear());
-		System.out.println(myDate3.getYear());
-		System.out.println(myDate4.getYear());
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss G").format(myDate));
 	}
 }
